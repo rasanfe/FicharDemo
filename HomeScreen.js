949ee2b3e9fa032181dbe5b3ef6e0667c90b4af9
Ramón San Félix Ramón <rasanfe82@gmail.com>
2024-10-19 10:07:17 +0200

    Retoco codigo

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,35 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { View, Image, ActivityIndicator, StyleSheet, Text } from 'react-native';
-import useAuth from './hooks/useAuth';
 import useNavigationBarVisibility from './hooks/useNavigationBarVisibility';
-
-
+import useAuth from './hooks/useAuth';
 
 const HomeScreen = ({ navigation }) => {
   const { isLogin, handleAutoLogin } = useAuth(navigation);
-  const [isConfigChecked, setIsConfigChecked] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
-  
   useNavigationBarVisibility(false);
 
   useEffect(() => {
-    const checkConfig = async () => {
-      const apiEndpoint = 'https://www.rsrsystem.com/ficharapidemo/api';
-
-      if (!apiEndpoint) {
-        // Navegar a la pantalla de Configuración si no existe la configuración
-        navigation.replace('Config');
-      } else {
-        // Continuar con el flujo normal
-        setIsConfigChecked(true);
-      }
-    };
-
-    checkConfig();
-  }, [navigation]);
-
-  useEffect(() => {
-    if (isConfigChecked) {
       // Configura un temporizador para ejecutar handleAutoLogin después de 3 segundos
       const timer = setTimeout(() => {
         handleAutoLogin();
@@ -38,8 +17,8 @@ const HomeScreen = ({ navigation }) => {
 
       // Limpieza del temporizador cuando el componente se desmonte
       return () => clearTimeout(timer);
-    }
-  }, [isConfigChecked, handleAutoLogin]);
+
+  },);
 
   return (
     <View style={styles.container}>
@@ -55,7 +34,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#ffffff',
+    backgroundColor: '#ffffff', 
   },
   image: {
     width: '90%',
